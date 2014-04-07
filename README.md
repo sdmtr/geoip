eab51ccba4acd2832e616bf25e92f53e622065c9
Savvas Dimitriou <savvas@sdmtr.com>
2014-04-07 11:13:17 +0930

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,101 @@
-geoip
+GeoIP
+GeoIP
 =====
 
-GeoIP PHP Class
+The GeoIP PHP class provides a quick way to use the information inferred by mod_geoip2 about the current visitor based on the MaxMind geoip database.
+
+Since we're using the free version of the database, we're limited to the following basic geoip information about the current visitor:
+
+>`GeoIP::country_code()`: returns a two-letter country code
+>
+>`GeoIP::country_name()`: returns the full country name
+>
+>`GeoIP::continent_code()`: returns a two-letter continent code
+>
+>`GeoIP::addr()`: returns the IP address mod_geoip2 is analysing
+>
+>`GeoIP::ip()`: attempts to figure out if the user is behind a proxy, and what the original IP might be
+>
+>`GeoIP::is_blocked()`: determine if the current visitor is on a list of blocked countries (which you can configure to suit your needs)
+
+For more information, see http://dev.maxmind.com/geoip/legacy/mod_geoip2/
+
+## Installing on CentOS 5 / 6
+
+### Enable the EPEL repository
+
+If the EPEL repository is already enabled, you can skip this step.
+
+#### RHEL / CentOS 5
+
+```
+# wget http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+# rpm -Uvh epel-release-5-4.noarch.rpm
+Preparing...                ########################################### [100%]
+   1:epel-release           ########################################### [100%]
+```
+
+#### RHEL / CentOS 6
+
+```
+# wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm
+# rpm -Uvh epel-release-6-7.noarch.rpm
+Preparing...                ########################################### [100%]
+   1:epel-release           ########################################### [100%]
+```
+   
+### Install mod_geoip with yum
+
+```
+# yum install mod_geoip GeoIP GeoIP-devel GeoIP-data zlib-devel
+```
+
+### Download the latest GeoIP database
+
+Once you've installed everything with yum the directory /usr/share/GeoIP should already contain the latest available version of GeoIP.dat, but you should update it just in case.
+
+```
+# cd /usr/share/GeoIP/
+# mv GeoIP.dat GeoIP.dat.old
+# wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+# gunzip GeoIP.dat.gz
+```
+
+### Enable mod_geoip2 in Apache
+
+In your apache conf file (usually located in `/etc/httpd/conf/httpd.conf`), add the following block at the end of the file:
+
+```
+<IfModule mod_geoip.c>
+GeoIPEnable On
+GeoIPDBFile /usr/share/GeoIP/GeoIP.dat
+</IfModule>
+```
+
+### Restart apache
+
+Usually like this:
+
+```
+# /etc/init.d/httpd restart
+```
+
+or
+
+```
+# service httpd restart
+```
+
+----
+
+All done! You should now be able to use the GeoIP class in your PHP scripts like so:
+
+```
+<?php
+
+  require_once '/path/to/geoip.php';
+  
+  print "Your country code is " . GeoIP::country_code();
+  
+?>
+```
---
+++ geoip.php
@@ -0,0 +1,107 @@
+<?php
+
+  /**
+   * Interface to mod_geoip2 
+   * 
+   * Provides a quick way to harness the information inferred by mod_geoip2
+   * about the current visitor based on the maxmind geoip database. For more
+   * information, see http://dev.maxmind.com/geoip/legacy/mod_geoip2/ and 
+   * 
+   * @author Savvas Dimitriou <savvas@sdmtr.com>
+   * @copyright Public Domain 
+   * @license http://creativecommons.org/publicdomain/zero/1.0/
+   * 
+   */
+  class GeoIP {
+    /**
+     * See http://en.wikipedia.org/wiki/ISO_3166-1
+     */
+    private static $_blocked_country_code = array('NG',  # nigeria
+                                                  'EG',  # egypt
+                                                  'IR',  # iran
+                                                  'AF'); # afghanistan
+
+    /**
+     * Returns a two-character country code for the current visitor.
+     * 
+     * In addition to the ISO 3166-1 country codes (available at
+     * http://en.wikipedia.org/wiki/ISO_3166-1), may also return:
+     * A1 – an anonymous proxy.
+     * A2 – a satellite provider.
+     * EU – an IP in a block used by multiple European countries.
+     * AP – an IP in a block used by multiple Asia/Pacific region countries.
+     * 
+     * @return string
+     */
+    public static function country_code() {
+      if (getenv('GEOIP_COUNTRY_CODE')) return getenv('GEOIP_COUNTRY_CODE');
+      else return null;
+    }
+
+    /**
+     * Returns the full country name for the current visitor.
+     * @return type
+     */
+    public static function country_name() {
+      if (getenv('GEOIP_COUNTRY_NAME')) return getenv('GEOIP_COUNTRY_NAME');
+      else return null;
+    }
+
+    /**
+     * Returns a two-character continent code for the current visitor.
+     * 
+     * AF – Africa
+     * AS – Asia
+     * EU – Europe
+     * NA – North America
+     * OC – Oceania
+     * SA – South America
+     * 
+     * @return string
+     */
+    public static function continent_code() {
+      if (getenv('GEOIP_CONTINENT_CODE')) return getenv('GEOIP_CONTINENT_CODE');
+      else return null;
+    }
+
+    /**
+     * Returns the IP address that mod_geoip2 is analysing, may differ from
+     * REMOTE_HOST due to mod_geoip2's ability to detect proxy information.
+     * @return string
+     */
+    public static function addr() {
+      if (getenv('GEOIP_ADDR')) return getenv('GEOIP_ADDR');
+      else return null;
+    }
+
+    /**
+     * Checks the current country code against the list of blocked country
+     * codes defined in this class file.
+     * @return bool
+     */
+    public static function is_blocked() {
+      if (getenv('GEOIP_COUNTRY_CODE'))
+        return in_array(getenv('GEOIP_COUNTRY_CODE'), self::$_blocked_countries);
+      else
+        return false;
+    }
+
+    /**
+     * Attempts to work out if the user is behind a proxy, and what the correct
+     * IP address might be.
+     * @return array
+     */
+    public static function ip() {
+      if (getenv('HTTP_X_FORWARDED_FOR')) {
+        $proxy = true;
+        $originalip = getenv('HTTP_X_FORWARDED_FOR');
+        $proxyip = getenv('REMOTE_ADDR');
+      } else {
+        $proxy = false;
+        $originalip = getenv('REMOTE_ADDR');
+        $proxyip = null;
+      }
+
+      return array('proxy' => $proxy, 'ip' => $originalip, 'proxyip' => $proxyip);
+    }
+  }
